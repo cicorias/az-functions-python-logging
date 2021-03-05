712ce46db2c90305e29f661fca15e811dbc08bc5
Shawn Cicoria <github@cicoria.com>
2021-03-05 16:23:47 -0500

    add rest of functions

--- logmessage/LoggingHelper.py
+++ logmessage/LoggingHelper.py
@@ -4,29 +4,47 @@ from typing import Dict, Optional, Any, MutableMapping
 from opencensus.ext.azure.log_exporter import AzureLogHandler
 import azure.functions as func
 
-class CustomLogger(logging.Logger):
-    def __init__(self, name: str, prefix: str) -> None:
-        LOGLEVEL = os.environ.get("LOGLEVEL", "WARNING").upper()
-        super().__init__(name, level=LOGLEVEL)
-        self.prefix = prefix
-
-
 
 class LoggingHelper(logging.LoggerAdapter):
     def __init__(self, prefix: str, logger: logging.Logger):
         super(LoggingHelper, self).__init__(logger, {})
         self.prefix = prefix
-    # def extra(self, extra: Dict[str, Dict[str, Any]]):
-    #     self.extra = extra
 
     def process(self, msg: str, kwargs: MutableMapping[str, Any]):
         return "[%s] %s" % (self.prefix, msg), kwargs
 
+    def debug(self, msg: str, context: func.Context = None, correlation_id: str = None, record: Dict[str, str] = None) -> None:
+        extra = self._get_logging_properties(context, correlation_id, record)
+        return super().debug(msg, extra=extra)
 
-    def info(self, msg: Any, context: func.Context, properties: Dict[str, str] = None) -> None:
-
-        extra=get_logging_properties({"CorrelationId": "file/one/two/three", "invocationidd": context.invocation_id})
+    def info(self, msg: str, context: func.Context = None, correlation_id: str = None, record: Dict[str, str] = None) -> None:
+        extra = self._get_logging_properties(context, correlation_id, record)
         return super().info(msg, extra=extra)
+
+    def warn(self, msg: str, context: func.Context = None, correlation_id: str = None, record: Dict[str, str] = None) -> None:
+        extra = self._get_logging_properties(context, correlation_id, record)
+        return super().warn(msg, extra=extra)
+
+    def error(self, msg: str, context: func.Context = None, correlation_id: str = None, record: Dict[str, str] = None) -> None:
+        extra = self._get_logging_properties(context, correlation_id, record)
+        return super().error(msg, extra=extra)
+
+    def exception(self, msg: str, context: func.Context = None, correlation_id: str = None, record: Dict[str, str] = None) -> None:
+        extra = self._get_logging_properties(context, correlation_id, record)
+        return super().exception(msg, extra=extra)
+
+    def _get_logging_properties(self, context: func.Context, correlation_id: str, record: Dict[str, str]):
+        properties = {
+            "custom_dimensions": {
+                "CorrelationId": correlation_id if correlation_id is not None else "-- not provided --",
+                "invocationidd": context.invocation_id if context is not None else "-- no context --",
+            }
+        }
+
+        if record is not None and isinstance(record, dict):
+            properties["custom_dimensions"].update(record)
+
+        return properties
 
 
 def get_logger(prefix: str, name: Optional[str] = None) -> LoggingHelper:
@@ -52,13 +70,3 @@ def get_logger(prefix: str, name: Optional[str] = None) -> LoggingHelper:
     log_adapter = LoggingHelper(prefix, logger)
     log_adapter.warn(f"logging set to {LOGLEVEL}")
     return log_adapter
-
-
-def get_logging_property(k: str, val: str) -> Dict[str, Dict[str, str]]:
-    properties = {"custom_dimensions": {k: val}}
-    return properties
-
-
-def get_logging_properties(d: Dict[str, str]):
-    properties = {"custom_dimensions": d}
-    return properties
--- logmessage/main.py
+++ logmessage/main.py
@@ -1,16 +1,18 @@
 import azure.functions as func
 import datetime
-import logging
-from .LoggingHelper import get_logger, get_logging_properties
-
-root_logger = logging.getLogger()
+from .LoggingHelper import get_logger
 
 logger = get_logger("breathe.fn_logmessage")
 
+
 def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
-    
-    logger.info(f"received Http Trigger for {context.invocation_id}", context)
-    
+
+    logger.info(f"HTTP trigger - no correlation ID {context.invocation_id}", context)
+
+    logger.info(f"HTTP trigger - correlation id {context.invocation_id}", context, "/zip/file/here")
+
+    logger.info(f"HTTP Trigger with additiona properties {context.invocation_id}", context, "filezip....json", record={"foo": "bar", "one": "two"})
+
     # logger.info('Python HTTP trigger function processed a request.', extra=get_logging_properties({"CorrelationId": "file/one/two/three", "invocationidd": context.invocation_id}))
     # root_logger.info("[breathe] - from root logger", extra=get_logging_properties({"CorrelationId": "file/one/two/three", "invocationidd": context.invocation_id}))
 
@@ -19,19 +21,19 @@ def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
     # logger.error(f"this is a Error message {context.invocation_id}")
     # logger.exception(f"this is a Exception Message message {context.invocation_id}") # , Exception("an exception example"))
 
-    message = req.params.get('message')
+    message = req.params.get("message")
     if not message:
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
-            message = req_body.get('message')
+            message = req_body.get("message")
 
     if message:
         return func.HttpResponse(f"Your message -- at {datetime.datetime.now()} - {message}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a message in the query string or in the request body for a personalized response.",
-             status_code=200
+            "This HTTP triggered function executed successfully. Pass a message in the query string or in the request body for a personalized response.",
+            status_code=200,
         )
